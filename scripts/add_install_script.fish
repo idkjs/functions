d30b846eff14a81bbcbe930d035dd02fc5b2d535
Alain Armand <aarmand.inbox@gmail.com>
2020-12-05 18:42:31 +0100

    fix install script in scripts

---
+++ scripts/add_install_script.fish
@@ -0,0 +1,12 @@
+# set license read ./LICENSE.md
+# echo $license
+set DIR $PWD
+echo $DIR
+echo $DIR/install.fish
+while read -la line
+    set currentFile $DIR/install.fish
+    echo "fisher install idkjs/$line" >>$currentFile
+end < $DIR/functions.txt
+
+
+
