8965433de31cc2fa33660a5c42fac8d42f64c2c1
Alain Armand <aarmand.inbox@gmail.com>
2020-12-06 01:10:50 +0100

    port to fish:WIP

--- scripts/update_functions.fish
+++ scripts/update_functions.fish
@@ -4,8 +4,8 @@ set script_dir (realpath (dirname (status --current-filename)))
 while read -la PROJECT_NAME
     echo $PROJECT_NAME
     cd $PROJECT_NAME
-    git add .
-    git commit -m "port to fish:WIP"
+    # git add .
+    git commit -a -m "port to fish:WIP"
     git push origin main
     cd $script_dir
 
